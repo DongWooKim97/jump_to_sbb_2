b2df692c112fd310ac9c5775b100100add085bc7
김동우 <nanaukim@naver.com>
2023-03-29 11:26:16 +0900

    Complete Chapter 2

---
+++ src/main/java/com/lion/sbbprac/MainController.java
@@ -0,0 +1,22 @@
+package com.lion.sbbprac;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "welcome to sbb";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+}
---
+++ src/main/java/com/lion/sbbprac/SbbpracApplication.java
@@ -0,0 +1,13 @@
+package com.lion.sbbprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbpracApplication.class, args);
+	}
+
+}
