3080769f91a0457dc85c12e65841d8f574e9ef39
김동우 <nanaukim@naver.com>
2023-03-29 13:25:43 +0900

    3-03 페이징 완료

--- src/test/java/com/lion/sbbprac/SbbpracApplicationTests.java
+++ src/test/java/com/lion/sbbprac/SbbpracApplicationTests.java
@@ -1,8 +1,10 @@
 package com.lion.sbbprac;
 
 import com.lion.sbbprac.answer.AnswerRepository;
+import com.lion.sbbprac.answer.AnswerService;
 import com.lion.sbbprac.question.Question;
 import com.lion.sbbprac.question.QuestionRepository;
+import com.lion.sbbprac.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,19 +22,16 @@ class SbbpracApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 
+	@Autowired
+	private QuestionService questionService;
+
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 
 }
